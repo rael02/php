86800b664053cd0c053ef9a3e2741ce238744dfd
Rael Lima <raellima01@outlook.com>
2017-05-09 19:35:34 -0300

    Create exemplo1.php

---
+++ exemplo1.php
@@ -0,0 +1,12 @@
+< Html >
+< Head >
+< Meta  charset = " utf-8 " />
+</ Head >
+< Body >
+<? Php
+$ Idade = 15 ;
+$ Nome  = " Rael " ;
+Echo  " Olá, meu nome é " , $ nome , " <br> Minha idade é " , $ idade ;
+? >
+</ Body >
+</ Html >
