3310bf0fb78df3f8ff20bd1dccfcef7b01901f83
Rael Lima <raellima01@outlook.com>
2017-05-16 20:31:32 -0300

    Create exemplo_14_2.php

---
+++ exemplo_14_2.php
@@ -0,0 +1,19 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+< H1 > Exemplo 14.2 </ h1 >
+<? Php
+Função  testeGlobal () {
+Global  $ a ;
+$ A = 25 ;
+}
+Echo " <h1> variável Global </ h1> " ;
+$ A = 0 ;
+TesteGlobal ();
+Echo " Valor dá Variável a = $ a " ;
+? >
+
+</ Body >
+</ Html >
