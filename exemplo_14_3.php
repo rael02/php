41824f25a1b6fa0982f686b1480b2c1c56a28e79
Rael Lima <raellima01@outlook.com>
2017-05-16 20:32:36 -0300

    Create exemplo_14_3.php

---
+++ exemplo_14_3.php
@@ -0,0 +1,20 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+< H1 > Exemplo 14.3 </ h1 >
+<? PHP
+Função  testeStatic () {
+Static  $ a = 0 ;
+$ A  + =  25 ;
+Echo  " Valor dá Variável a = $ a <br> " ;
+}
+Echo " <h1> Variável estática </ h1> " ;
+TesteStatic ();
+TesteStatic ();
+TesteStatic ();
+? >
+
+</ Body >
+</ Html >
