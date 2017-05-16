72e5cd43753209e838a7cef229fb7273cbbc8120
Rael Lima <raellima01@outlook.com>
2017-05-16 20:13:21 -0300

    Create exemplo4.php

---
+++ exemplo4.php
@@ -0,0 +1,17 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+<? Php
+Echo " <h1> ******** Operadores de Cordas ******* </ h1> <br> " ;
+$ A = " Bem Vindo " ;
+$ B = " ao curso " ;
+$ C = " de informática " ;
+$ D = $ a . $ b . $ C. = " Dá disciplina de PHP </ br> " ;
+Echo  $ d ;
+$ A. = " Aluno " ;
+Echo  $ a ;
+? >
+</ Body >
+</ Html >
