b52ba5814393f9c46b5d6716a202ca8a5350851d
Rael Lima <raellima01@outlook.com>
2017-05-16 20:29:14 -0300

    Create exemplo_13.php

---
+++ exemplo_13.php
@@ -0,0 +1,17 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+< H1 > Exemplo 13 </ h1 >
+<? Php
+Função  soma ( $ n1 , $ n2 ) {
+Return  $ n1 + $ n2 ;
+} Função  imprimir () {
+Return " O resultado dá soma é: " ;
+}
+Echo imprimir () . Soma ( 115 , 30 );
+? >
+
+</ Body >
+</ Html >
