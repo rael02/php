0671f8eacb8b0833032d2bc7ffee52e436c96d63
Rael Lima <raellima01@outlook.com>
2017-05-16 20:27:59 -0300

    Create exemplo_12.php

---
+++ exemplo_12.php
@@ -0,0 +1,20 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+<? Php
+Função  msn () {
+$ Msn = " Olá Bem Vindo como Funções de PHP " ;
+ Echo " <h2> $ msn </ h2> " ;
+ }
+ Função  imprimir ( $ texto ) {
+ Echo " <h3> $ texto </ h3> " ;
+ }
+ Msn ();
+Imprimir ( " testando função " );
+ 
+? >
+
+</ Body >
+</ Html >
