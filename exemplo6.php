ffd68ff70e71f303944da7f4004348d96d1ff83b
Rael Lima <raellima01@outlook.com>
2017-05-16 20:15:36 -0300

    Create exemplo6.php

---
+++ exemplo6.php
@@ -0,0 +1,28 @@
+< Html >
+< Head >
+< Título > Exemplo 6 </ title >
+</ Head >
+< Body >
+<? Php
+" <H1> Operadores booleanos </ h1> <br> " ;
+$ C =  FALSE ;
+Echo  " 12> = 12 e 10> 2 " , ( 12 > = 12  e  10 > 2 )?
+" Verdadeiro " : " falso " ;
+Echo " <br> " ;
+Echo  " 12> = 12 ou FALSE " , ( 12 > = 12  ou  $ c )?
+" Verdadeiro " : " falso " ;
+Echo " <br> " ;
+Echo  " 12> = 12 xor 10> 2 " , ( 12 > = 12  xor  10 > 2 )?
+" Verdadeiro " : " falso " ;
+Echo " <br> " ;
+Echo  " ! FALSE " , ( $ c )?
+" Verdadeiro " : " falso " ;
+Echo " <br> " ;
+Echo  " 10> 2 && FALSE " , ( 10 > 2  &&  FALSE )?
+" Verdadeiro " : " falso " ;
+Echo " <br> " ;
+echo "12>=12 || FALSE",(12>=12 || FALSE) ?
+"verdadeiro" : "falso";
+?>
+</body>
+</html>
