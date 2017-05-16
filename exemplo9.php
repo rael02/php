b2ccc3214b30a92d5c9954d6755c1649791996b7
Rael Lima <raellima01@outlook.com>
2017-05-16 20:24:23 -0300

    Create exemplo9.php

---
+++ exemplo9.php
@@ -0,0 +1,13 @@
+< Html >
+< Head >
+< Título > Exemplo 9 </ title >
+</ Head >
+< Body >
+< H1 > Estrutura de Repetição (For) </ h1 >
+<? Php
+Para ( $ i = 1 ; $ i <= 10 ; $ i ++ ) {
+Echo " <b> Número: $ i <br> " ;
+}
+? >
+</ Body >
+</ Html >
