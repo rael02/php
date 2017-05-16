4090125b32253f0fc1d277e3a1a0f66b5f5ba764
Rael Lima <raellima01@outlook.com>
2017-05-16 20:25:31 -0300

    Create exemplo_10.php

---
+++ exemplo_10.php
@@ -0,0 +1,15 @@
+< Html >
+< Head >
+< Título > Exemplo 10 </ title >
+</ Head >
+< Body >
+< H1 > Estrutura de Repetição (while) </ h1 >
+<? Php
+$ Num = 1 ;
+Enquanto ( $ num <= 10 ) {
+Echo " Número: $ num <br> " ;
+$ Num ++ ;
+}
+? >
+</ Body >
+</ Html >
