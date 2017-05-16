c9825b17fa58f60627ee89a6f7c62b0c233faf32
Rael Lima <raellima01@outlook.com>
2017-05-16 20:12:07 -0300

    Create exemplo3.php

---
+++ exemplo3.php
@@ -0,0 +1,38 @@
+< Html >
+< Head >
+< Título > Exemplo 3 </ title >
+</ Head >
+< Body >
+<? Php
+Echo  " <h1> Operadores Matemáticos </ h1> <br> " ;
+$ X =  2 ;
+Echo ( $ x + 2 );
+Echo  " <br> " ;
+$ X =  2 ;
+Eco ( 5 - $ x );
+Echo " <br> " ;
+$ X =  4 ;
+Echo ( $ x * 5 );
+Echo " <br> " ;
+$ X =  15 ;
+Echo ( $ x / 5 );
+Echo " <br> " ;
+$ X =  10 ;
+Echo ( $ x % 2 );
+Echo  " <br> " ;
+Echo " <br> <br> <br> <h2> Operadores de decremento e incremento </ h2> <br> " ;
+$ A =  1 ;
+Echo  ++ $ a ;
+Echo " <br> " ;
+Echo  $ a ++ ;
+Echo " <br> " ;
+Echo  $ a ;
+Echo  " <br> " ;
+Echo  - $ a ;
+Echo  " <br> " ;
+Echo  $ a - ;
+Echo  " <br> " ;
+Echo  $ a ;
+? >
+</ Body >
+</ Html >
