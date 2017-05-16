591480470c6b6b113f4a30c23acbd31d5195c54f
Rael Lima <raellima01@outlook.com>
2017-05-16 20:19:40 -0300

    Create exemplo7_2.php

---
+++ exemplo7_2.php
@@ -0,0 +1,18 @@
+< HTML >
+< HEAD >
+< Meta  charset = " utf-8 " />
+</ HEAD >
+< BODY >
+< H1 > Estrutura condicional (IF / ELSE) </ h1 >
+<? Php
+$ Idade = 12 ;
+$ Maioridade = 18 ;
+se ( $ Idade > = $ maioridade ) {
+echo ( " <br> <b> " + $ Idade . " anos, rápido Você já E Maior de Idade </ b> " );
+}
+Else {
+echo ( " <br> <b> " + $ Idade . " anos, rápido Você AINDA Não E Maior de Idade </ b> " );
+}
+? >
+</ BODY >
+</ HTML >
