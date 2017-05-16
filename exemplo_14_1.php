b91f502f237a3ace23cd20f760e0a2cf41a27918
Rael Lima <raellima01@outlook.com>
2017-05-16 20:30:16 -0300

    Create exemplo_14_1.php

---
+++ exemplo_14_1.php
@@ -0,0 +1,17 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+< H1 > Exemplo 14.1 </ h1 >
+<? Php
+Função  testeLocal () {
+$ A = 25 ;
+}
+Echo " <h1> Variável Local </ h1> " ;
+TesteLocal ();
+Echo  $ a = 25 ;
+? >
+
+</ Body >
+</ Html >
