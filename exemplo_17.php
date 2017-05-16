c8ff4b1e8e227a04a329769774f181eb9a5390f4
Rael Lima <raellima01@outlook.com>
2017-05-16 20:36:05 -0300

    Create exemplo_17.php

---
+++ exemplo_17.php
@@ -0,0 +1,23 @@
+< Html >
+< Head >
+< Título > Exemplo 17 </ title >
+</ Head >
+< Body >
+<? Php
+Echo " <h1> Lendo um vetor </ h1> " ;
+$ Frutas = array ( " Laranja " , " Maçã " , " Uva " , " Banana " , " Mamão " );
+Foreach ( $ frutas  como  $ valor ) {
+Echo " <li> Fruta: $ valor </ li> " ;
+}
+Echo " <br> " ;
+$  Dados =  array ( " nome " => " Maria Cristina " ,
+" Rua " => " São João " ,
+" Bairro " => " Cidades dos Campos " ,
+" Cidade " => " São Paulo " );
+Foreach ( $ dados  como  $ chave  =>  $ valor )
+{
+Echo " $ chave : $ valor " ;
+}
+? >
+</ Body >
+</ Html >
