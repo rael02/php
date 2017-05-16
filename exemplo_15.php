99308eaaf9441040798c29a41e3523707de3251b
Rael Lima <raellima01@outlook.com>
2017-05-16 20:33:31 -0300

    Create exemplo_15.php

---
+++ exemplo_15.php
@@ -0,0 +1,18 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+< H1 > Exemplo 15 </ h1 >
+<? PHP
+Função  mais5 ( $ numero ) {
+$ Numero  + =  5 ;
+}
+Echo " <h1> Passagem do parâmetro </ h1> " ;
+$ A = 3 ;
+Mais5 ( $ a );
+Echo " Valor Variável a = $ a <br> " ;
+? >
+
+</ Body >
+</ Html >
