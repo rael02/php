fe20d21286a80ca53cf85c3342e623029eee061b
Rael Lima <raellima01@outlook.com>
2017-05-16 20:18:41 -0300

    Create exxemplo7_1.php

---
+++ exxemplo7_1.php
@@ -0,0 +1,15 @@
+< HTML >
+< HEAD >
+</ HEAD >
+< BODY >
+<? Php
+$ Opc = 2 ;
+se ( OPC $ == 1 ) {
+Echo ( " <br> Opção escolha 1 </ b> " );
+}
+se ( $ OPC == 2 ) {
+echo("<br><b> Opção escolhida 2 </b>");
+}
+?>
+</BODY>
+</HTML>
