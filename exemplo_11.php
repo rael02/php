17982749dc51af3c1686e138acdc3cb507b239db
Rael Lima <raellima01@outlook.com>
2017-05-16 20:26:55 -0300

    Create exemplo_11.php

---
+++ exemplo_11.php
@@ -0,0 +1,15 @@
+< Html >
+< Head >
+< Título > Exemplo 11 </ title >
+</ Head >
+< Body >
+< H1 > Estrutura de Repetição </ h1 >
+<? Php
+$ Num = 10 ;
+fazer {
+Echo " número: $ num <br> " ;
+$ Num - ;
+} While ( $ num > = 1 )
+? >
+</ Body >
+</ Html >
