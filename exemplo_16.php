af61b96878684caf0ba1ac3dde1d51ca5ebde29e
Rael Lima <raellima01@outlook.com>
2017-05-16 20:34:24 -0300

    Create exemplo_16.php

---
+++ exemplo_16.php
@@ -0,0 +1,16 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+< H1 > Exemplo 16 </ h1 >
+<? PHP
+Função  teste ( $ txt  =  " testando " ) {
+Echo " <h2> $ txt </ h2> " ;
+}
+Teste ();
+Teste ( " Outro Teste " );
+? >
+
+</ Body >
+</ Html >
