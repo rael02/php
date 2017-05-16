2d808b3a4d3757916450d5e6dff107048d19aef0
Rael Lima <raellima01@outlook.com>
2017-05-16 20:14:33 -0300

    Create exemplo5.php

---
+++ exemplo5.php
@@ -0,0 +1,29 @@
+< Html >
+< Head >
+< Title > </ title >
+</ Head >
+< Body >
+<? PHP
+Echo  " <h1> operadores de Comparação <br> </ h1> " ;
+$ A = 15 ;
+$ B = 42 ;
+$ C = 42,0 ;
+Echo  " $ b == $ c " , $ b == $ c ? " Verdadeiro " : " falso " ;
+Echo  " <br> " ;
+Echo  " $ b === $ c " , $ b === $ c ? " Verdadeiro " : " falso " ;
+Echo  " <br> " ;
+Echo  " $ b ! = $ C " , $ b ! = $ C ? " Verdadeiro " : " falso " ;
+Echo  " <br> " ;
+Echo  " $ a <> $ c " , $ a <> $ c ? " Verdadeiro " : " falso " ;
+Echo  " <br> " ;
+Echo  " $ a < $ c " , $ a > $ c ? " Verdadeiro " : " falso " ;
+Echo  " <br> " ;
+Echo  " $ a > $ c " , $ a > $ c ? " Verdadeiro " : " falso " ;
+Echo  " <br> " ;
+Echo  " $ a <= $ c " , $ a <= $ c ? " Verdadeiro " : " falso " ;
+Echo  " <br> " ;
+Echo  " $ a > = $ c " , $ a > = $ c ? " Verdadeiro " : " falso " ;
+? >
+</ PHP >
+</ Body >
+</ Html >
