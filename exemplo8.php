3544e963db8f87848fd61a2d443e1f7552d0e923
Rael Lima <raellima01@outlook.com>
2017-05-30 18:50:03 -0300

    Update exemplo8.php

--- exemplo8.php
+++ exemplo8.php
@@ -1,27 +1,27 @@
-< Html >
-< Head >
-< Título > Exemplo 8 </ title >
-</ Head >
-< Body >
-<? Php
-Echo  " <h1> Estrutura condicional (switch) </ h1> " ;
-$ Dia =  getdate ();
-Switch ( $ dia [ " wday " ]) {
-Caso  5 :
-Echo  " finalmente Sexta " ;
-quebrar ;
-Caso  6 :
-Echo " super sábado " ;
-quebrar ;
-Caso  0 ;
-Echo " domingo sonolento " ;
-quebrar ;
-Caso  2 ;
-Echo " Aula de Programação " ;
-quebrar ;
-Padrão ;
-Echo " Estou esperando Final de Semana " ;
+<html>
+<head>
+<title>exemplo 8</title>
+</head>
+<body>
+<?php
+echo "<h1>Estrutura condicional(switch)</h1>";
+$dia= getdate();
+switch($dia["wday"]){
+case 5:
+echo "finalmente Sexta";
+break;
+case 6:
+echo"super sábado";
+break;
+case 0;
+echo"domingo sonolento";
+break;
+case 2;
+echo"Aula de Programação";
+break;
+default;
+echo"Estou esperando Final de Semana";
 }
-? >
-</ Body >
-</ Html >
+?>
+</body>
+</html>
