239986a933e6598d1647196042feaa8cb6fcaee9
Rael Lima <raellima01@outlook.com>
2017-05-30 18:40:03 -0300

    Update exemplo_14_2.php

--- exemplo_14_2.php
+++ exemplo_14_2.php
@@ -1,19 +1,19 @@
-< Html >
-< Head >
-< Title > </ title >
-</ Head >
-< Body >
-< H1 > Exemplo 14.2 </ h1 >
-<? Php
-Função  testeGlobal () {
-Global  $ a ;
-$ A = 25 ;
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<h1>Exemplo 14.2</h1>
+<?php
+function testeGlobal(){
+global $a;
+$a=25;
 }
-Echo " <h1> variável Global </ h1> " ;
-$ A = 0 ;
-TesteGlobal ();
-Echo " Valor dá Variável a = $ a " ;
-? >
+echo"<h1>variável Global</h1>";
+$a=0;
+testeGlobal();
+echo"Valor dá Variável a = $a";
+?>
 
-</ Body >
-</ Html >
+</body>
+</html>
