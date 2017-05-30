93dc7812b580e235dbc18f8422dadeaba01553aa
Rael Lima <raellima01@outlook.com>
2017-05-30 18:29:29 -0300

    Update exemplo_19.php

--- exemplo_19.php
+++ exemplo_19.php
@@ -1,18 +1,18 @@
-< Html >
-< Head >
-	< Meta  charset = ' utf-8 ' >
+<html>
+<head>
+	<meta charset='utf-8'>
 
-</ Head >
-< Body >
-	< Form  method = ' get '  action = '  ' >
-	Login: < entrada  tipo = ' texto '  size = ' 20 '  name = ' logon ' > < br >
-	Senha: < input  type = ' password '  size = ' 20 '  name = ' senha ' >
-	< Entrada  tipo = ' enviar '  valor = ' Enviar ' > < br >
-	<? Php
-	 se ( isset ( $ _GET [ ' Login ' ])) {
-	 	Echo  $ _GET [ ' login ' ] . ' <br> ' ;
-	 	Echo  $ _GET [ ' senha ' ] . ' <br> ' ;
+</head>
+<body>
+	<form method='get' action=' '>
+	Login: <input type='text' size='20' name='login'><br>
+	Senha: <input type='password' size='20' name='senha'>
+	<input type='submit' value='Enviar'><br>
+	<?php
+	 if(isset($_GET['login'])){
+	 	echo $_GET['login'].'<br>';
+	 	echo $_GET['senha'].'<br>';
 	 }
-	? >
-</ Body >
-</ Html >
+	?>
+</body>
+</html>
