a9b3743886d075cedbcdd6bfcb82fa51f75f05ff
Rael Lima <raellima01@outlook.com>
2017-05-30 18:37:01 -0300

    Update exemplo_15.php

--- exemplo_15.php
+++ exemplo_15.php
@@ -1,18 +1,18 @@
-< Html >
-< Head >
-< Title > </ title >
-</ Head >
-< Body >
-< H1 > Exemplo 15 </ h1 >
-<? PHP
-Função  mais5 ( $ numero ) {
-$ Numero  + =  5 ;
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<h1>Exemplo 15</h1>
+<?PHP
+function mais5($numero){
+$numero += 5;
 }
-Echo " <h1> Passagem do parâmetro </ h1> " ;
-$ A = 3 ;
-Mais5 ( $ a );
-Echo " Valor Variável a = $ a <br> " ;
-? >
+echo"<h1>Passagem de parâmetro</h1>";
+$a=3;
+mais5($a);
+echo"Valor Variável a = $a <br>";
+?>
 
-</ Body >
-</ Html >
+</body>
+</html>
