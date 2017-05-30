4d165d35dd36d29e8756aec8e52f37947362ea1a
Rael Lima <raellima01@outlook.com>
2017-05-30 18:56:05 -0300

    Update exemplo6.php

--- exemplo6.php
+++ exemplo6.php
@@ -1,28 +1,32 @@
-< Html >
-< Head >
-< Título > Exemplo 6 </ title >
-</ Head >
-< Body >
-<? Php
-" <H1> Operadores booleanos </ h1> <br> " ;
-$ C =  FALSE ;
-Echo  " 12> = 12 e 10> 2 " , ( 12 > = 12  e  10 > 2 )?
-" Verdadeiro " : " falso " ;
-Echo " <br> " ;
-Echo  " 12> = 12 ou FALSE " , ( 12 > = 12  ou  $ c )?
-" Verdadeiro " : " falso " ;
-Echo " <br> " ;
-Echo  " 12> = 12 xor 10> 2 " , ( 12 > = 12  xor  10 > 2 )?
-" Verdadeiro " : " falso " ;
-Echo " <br> " ;
-Echo  " ! FALSE " , ( $ c )?
-" Verdadeiro " : " falso " ;
-Echo " <br> " ;
-Echo  " 10> 2 && FALSE " , ( 10 > 2  &&  FALSE )?
-" Verdadeiro " : " falso " ;
-Echo " <br> " ;
-echo "12>=12 || FALSE",(12>=12 || FALSE) ?
-"verdadeiro" : "falso";
+<HTML>
+<HEAD>
+<TITLE>New Document</TITLE>
+</HEAD>
+<BODY>
+<h1>Estrutura condicional (if/else)</h1>
+<h2>Cálculo IMC</h2>
+<?php
+$altura=1.58; //Altura do indivíduo
+$peso=62; //Peso do indivíduo
+$imc=$peso/($altura*$altura); //Cálculo do IMC
+if($imc<18.5){
+echo ($imc+" Classificação magreza");
+}
+else if(($imc>18.5)&&($imc<24.9)){
+echo ($imc." Classificação saudável");
+}
+else if(($imc>24.9)&&($imc<29.9)){
+echo ($imc." Classificação sobrepeso");
+}
+else if(($imc>29.9)&&($imc<34.9)){
+echo ($imc." Classificação obesidade grau I");
+}
+else if(($imc>34.9)&&($imc<39.9)){
+echo ($imc." Classificação obesidade grau II(severa)");
+}
+else if($imc>=40){
+echo ($imc." Classificação obesidade grau III(mórbida)");
+}
 ?>
-</body>
-</html>
+</BODY>
+</HTML>
