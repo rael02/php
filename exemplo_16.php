446261f79d2cc4bc939c025e3d381feae110cea6
Rael Lima <raellima01@outlook.com>
2017-05-30 18:35:38 -0300

    Update exemplo_16.php

--- exemplo_16.php
+++ exemplo_16.php
@@ -1,16 +1,16 @@
-< Html >
-< Head >
-< Title > </ title >
-</ Head >
-< Body >
-< H1 > Exemplo 16 </ h1 >
-<? PHP
-Função  teste ( $ txt  =  " testando " ) {
-Echo " <h2> $ txt </ h2> " ;
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<h1>Exemplo 16</h1>
+<?PHP
+function teste($txt = "testando"){
+echo"<h2> $txt </h2>";
 }
-Teste ();
-Teste ( " Outro Teste " );
-? >
+teste();
+teste("Outro Teste");
+?>
 
-</ Body >
-</ Html >
+</body>
+</html>
